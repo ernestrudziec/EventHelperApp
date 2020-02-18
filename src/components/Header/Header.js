b1c76358b34283e9dd732f73e0bb991e1eb79675
ernestrudziec <ernest.rudziec@gmail.com>
2020-02-18 15:22:05 +0100

    fixed some mobile sizes
    fixed some mobile sizes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,21 @@
 import React from "react";
 import styles from "./Header.module.scss";
 import logoKum from "../../assets/logo/logo-kum.png";
+import logoSUWSPol from '../../assets/logo/logo-su-wspol.jpg';
+import logoWSPol from '../../assets/logo/logo-wspol.png';
+
 import Clock from "../Clock/Clock";
 
 const Header = () => {
 
     return(
         <>
-        <img src= {logoKum} className={styles.logoKum} alt="Komisja Uczelni Mundurowych"/>
+            <div className={styles.logoWrapper}>
+
+            <img src= {logoSUWSPol} className={styles.logoSUWSPol} alt="Komisja Uczelni Mundurowych"/>
+                <img src= {logoKum} className={styles.logoKum} alt="Komisja Uczelni Mundurowych"/>
+                <img src= {logoWSPol} className={styles.logoWSPol} alt="Komisja Uczelni Mundurowych"/>
+            </div>
         <h1 className={styles.mainTitle}> XXXVII Zjazd KUM </h1>
         <h2 className={styles.secondaryTitle}> Wyższa Szkoła Policji w Szczytnie</h2>
             <Clock/>
