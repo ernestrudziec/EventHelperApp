1474798e5f9d86ae2178619fade94e5befaa68a2
ernestrudziec <ernest.rudziec@gmail.com>
2020-02-26 15:06:22 +0100

    Fixed navigation buttons.
    Fixed navigation buttons.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -9,25 +9,25 @@ const Nav = () => {
         <div className={styles.wrapper}>
 
 
-            <ul className={styles.list}>
-                <li><NavLink exact activeClassName={styles.navItemLinkActive}
+            <div className={styles.list}>
+              <NavLink exact activeClassName={styles.navItemLinkActive}
                              className={styles.navItemLink} to = "/kontakt">
                     Kontakt
                     <div className="fas fa-phone-square-alt"></div>
                 </NavLink>
-                </li>
-                <li><NavLink exact activeClassName={styles.navItemLinkActive}
+
+              <NavLink exact activeClassName={styles.navItemLinkActive}
                              className={styles.navItemLink} to = "/planzjazdu">
                     Plan zjazdu
                     <div className="fas fa-calendar-check"></div>
                 </NavLink>
-                </li>
-                <li><NavLink exact activeClassName={styles.navItemLinkActive}
+
+               <NavLink exact activeClassName={styles.navItemLinkActive}
                              className={styles.navItemLink} to = "/mapawspol">Mapa WSPol
                     <div className="fas fa-map-pin"></div>
                 </NavLink>
-                </li>
-            </ul>
+
+            </div>
 
         </div>
 
